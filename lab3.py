90c23337893adb93f8bab45947fc1fa6f524af73
Aroon Tcholakov <91400861+aroontcholakov@users.noreply.github.com>
2022-03-07 22:55:17 -0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -5,3 +5,49 @@ import seaborn as sns
 import numpy as np
 import altair as alt
 
+df_data = pd.read_csv('C:/Users/aroon/OneDrive/Documents/Ohio State Academics stuff/CSE 5544/CSE5544.Lab1.ClimateData.csv', sep=',')
+df_data = df_data.drop(columns=['Non-OECD Economies'])
+df_data = df_data[df_data['Country\\year'].apply(lambda x: 'OECD' not in x and 'European Union' not in x)]
+cols = df_data.columns.drop('Country\\year')
+df_data[cols] = df_data[cols].apply(pd.to_numeric, errors='coerce')
+data = pd.melt(df_data, id_vars=['Country\year'], var_name='year')
+data = data.rename(columns={'Country\year': 'Country'})
+
+st.title('Lab 3: Climate Analysis')
+
+
+heatmap = alt.Chart(data).mark_rect().encode(
+    alt.X('year:O'),
+    alt.Y('Country:N', sort='ascending'),
+    alt.Color('value:Q', scale=alt.Scale(scheme='inferno')),
+    size='value:Q',
+    tooltip=['value']
+).properties(
+    title={
+        "text":['CO2 Emissions for Countries (1990-2019)'],
+        "subtitle":["Including Countries with both OECD and Non-OECD Economies"]
+    }
+).configure_title(
+    fontSize = 24,
+    font='Times New Roman',
+    anchor='start',
+)
+
+st.altair_chart(heatmap, use_container_width=False)
+# st.text("")
+
+
+scatterplot = alt.Chart(data).mark_circle().encode(
+    alt.X('year:O'),
+    alt.Y('value:Q', axis=alt.Axis(title='Emissions', labels=False), sort='descending'),
+    alt.Color('value:Q', scale=alt.Scale(scheme='sinebow')),
+    alt.Size('Country:N', legend=None),
+    opacity='value:Q',
+    tooltip=['Country']
+).properties(
+    title='Emissions from 1990 to 2019'
+)
+
+st.altair_chart(scatterplot, use_container_width=True)
+
+st.text("")
